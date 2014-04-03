806a6fbbf33c221f31b4a4c327ae9131e42ea664
Lukasz Konopski <konopski@gmail.com>
2014-04-03 18:03:29 +0200

    fix pretest script

--- pretest.sh
+++ pretest.sh
@@ -12,7 +12,7 @@ git branch || exit 1
 BRANCH=`git branch | grep "^*" | cut -d " " -f 2`
 pushd /tmp
 rm -rf $PRETEST_DIR
-git clone $PRETEST_DIR || exit 1
+git clone $HERE $PRETEST_DIR || exit 1
 cd $PRETEST_DIR
 git checkout $BRANCH
 $BUILD_CMD || exit 1
