a0eaf772b235cc32675bc56ceeda5c4536ab274c
Lukasz K <lkono@softserveinc.com>
2017-11-24 16:02:08 +0100

    add git presentation

---
+++ session.sh
@@ -0,0 +1,37 @@
+git clone https://github.com/konopski/myrepo.git projectd
+cd project0
+git log
+git hist
+git branch --all
+vim README.md
+git status
+git diff
+git stash
+git status
+git diff
+git stash list
+git stash pop
+git difff
+git add README.md
+git status
+git difff
+git difff --staged
+git commit -m "[TICKET] my beautyful commit"
+git hist
+git co -b feature/TICKET_whatever
+vim README.md 
+git st
+git difff
+git add README.md
+git commit -m "[TICKET] my ugly second commit"
+git hist
+git rebase -i HEAD~2 
+git hist
+git push
+git push origin feature/TICKET_whatever
+echo "http://rogerdudler.github.io/git-guide/"
+echo "https://learngitbranching.js.org/"
+echo "https://www.atlassian.com/git/tutorials"
+echo "https://github.com/jakubnabrdalik/gitkurwa"
+echo "http://fabioz.github.io/mu-repo/"
+echo "https://github.com/git-tips/tips"
---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+if doitlive play session.sh -p robbyrussell -S "c:\stuff\cmdr\vendor\git-for-windows\bin\bash.exe"
+then
+	rm -rf project0
+fi
